1f3b9524281b0e715c18c86c7c1c69f6139f77c3
felipe.aug.silva <felipe.aug.silva@4f9569d0-c5a3-a9bf-340a-531e8ef3e0f7>
2010-12-09 20:27:12 +0000

    Datapath OK!

--- sl2.vhd
+++ sl2.vhd
@@ -2,11 +2,12 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity sl2 is -- shift left by 2
-    port (a: in  STD_LOGIC_VECTOR (31 downto 0);
-          y: out STD_LOGIC_VECTOR (31 downto 0));
+    generic (W: integer);
+    port (a: in  STD_LOGIC_VECTOR (W-1 downto 0);
+          y: out STD_LOGIC_VECTOR (W-1 downto 0));
 end;
 
 architecture behave of sl2 is
 begin
-    y <= a(29 downto 0) & "00";
+    y <= a(W-3 downto 0) & "00";
 end;
--- tb_datapath.vhd
+++ tb_datapath.vhd
@@ -11,28 +11,29 @@ component datapath is
   port( clk, reset: in    STD_LOGIC;
         PCF:        inout STD_LOGIC_VECTOR(31 downto 0);  
         instr:      in    STD_LOGIC_VECTOR(31 downto 0); 
-        WriteRegW: in    STD_LOGIC_VECTOR(4 downto 0);
-        ResultW  :  in    STD_LOGIC_VECTOR(31 downto 0);   
-        RegWriteW: in    STD_LOGIC;
-        SaidaFlopD: out  STD_LOGIC_VECTOR(115 downto 0)); 
+        Data:  in  STD_LOGIC_VECTOR(31 downto 0); 
+        WriteDataM : out STD_LOGIC_VECTOR(31 downto 0); 
+        MemWriteM:  out STD_LOGIC);
 End component;
 
 
     signal clk                : STD_LOGIC := '0';
     signal reset              : STD_LOGIC := '1';    
     signal instr              : STD_LOGIC_VECTOR(31 downto 0);
-    signal saidaFlopD         : STD_LOGIC_VECTOR (115 downto 0);
-    signal  PCF               : STD_LOGIC_VECTOR(31 downto 0);  
-    signal WriteRegW          : STD_LOGIC_VECTOR(4 downto 0); 
-    signal ResultW            : STD_LOGIC_VECTOR(31 downto 0);
-    signal RegWriteW          : STD_LOGIC;
+    --signal saidaFlopD         : STD_LOGIC_VECTOR (115 downto 0);
+    signal PCF                : STD_LOGIC_VECTOR(31 downto 0);  
+    --signal WriteRegW          : STD_LOGIC_VECTOR(4 downto 0); 
+    --signal ResultW            : STD_LOGIC_VECTOR(31 downto 0);
+    --signal RegWriteW          : STD_LOGIC;
+    signal Data:    STD_LOGIC_VECTOR(31 downto 0); 
+    signal WriteDataM : STD_LOGIC_VECTOR(31 downto 0); 
+    signal MemWriteM:   STD_LOGIC;
+    --signal SaidaFlopM: STD_LOGIC_VECTOR(70 downto 0);
 
-    signal erro: boolean := false;  -- Para terminar a simulacao
     
     begin
 
-        datapath_0: datapath port map(clk, reset, PCF, instr, WriteRegW, ResultW,
-                                      RegWriteW, saidaFlopD);
+        datapath_0: datapath port map(clk, reset, PCF, instr, Data, WriteDataM, MemWriteM);
 
         process 
         begin
@@ -47,51 +48,54 @@ End component;
         begin
         --Seta valores aos registradores
             --wait for 100 ns;
-            instr <= "000100" & "00001" & "00010" & "0000000000100000"; -- beq com valores diferentes
-            RegWriteW <= '1';
-            for i in 0 to 31 loop
-              ResultW <= std_logic_vector(to_unsigned(i, 32));
-              WriteRegW  <= std_logic_vector(to_unsigned(i, 5));
-              wait for 100 ns;
-            end loop;
-            ResultW <= std_logic_vector(to_unsigned(3, 32));
-            WriteRegW  <= std_logic_vector(to_unsigned(4, 5));
-            RegWriteW <= '0';
-            --wait;
-            reset <= '0';
-            --wait for 3300 ns;
-            -- Clock
-            --clk <= not clk after 50 ns;      
 
+            instr <= "001000" & "00000" & "00000" & "0000000000000000"; -- addi 0
+
+            wait for 100 ns;
+            reset <= '0';
+
+            --wait FOR 100 ns;
+            --instr <= "001000" & "00000" & "00000" & "0000000000000000"; -- addi 0
+            --wait for 900 ns;
+
+            instr <= "100011" & "00000" & "00010" & "0000000000010000";
+            Data <= x"00000001";
+
+            wait FOR 100 ns;
+            instr <= "001000" & "00000" & "00000" & "0000000000000000"; -- addi 0
+            wait for 900 ns;
+
+            instr <= "000010" & "00000000000000000000000001";
+ 
+            --instr <= "000000" & "00001" & "00010" & "00011" & "00000" & "100000";
+            --RegWriteW <= '1';
+            --for i in 0 to 31 loop
+            --  ResultW <= std_logic_vector(to_unsigned(i, 32));
+            --  WriteRegW  <= std_logic_vector(to_unsigned(i, 5));
+            --  wait for 100 ns;
+            --end loop;
+            --ResultW <= std_logic_vector(to_unsigned(3, 32));
+            --WriteRegW  <= std_logic_vector(to_unsigned(4, 5));
+            --RegWriteW <= '0';
+
+            
+    
+
+
+            --instr <= "001000" & "00001" & "00011" & "0000000000000100"; -- addi
             --instr <= "100011" & "01000" & "01001" & "0000000000100000"; -- lw
                     --x"AD310004" after 100 ns, -- sw
             --wait for 100 ns;
-            instr <= "000100" & "00011" & "00011" & "0000000000100000"; -- beq com valores iguais
-            wait for 100 ns;
+            --instr <= "000100" & "00011" & "00011" & "0000000000100000"; -- beq com valores iguais
+            --wait for 100 ns;
 
-            instr <= "000100" & "00011" & "00011" & "0000000000000000"; -- beq com valores iguais
-            wait for 100 ns;
+            --instr <= "000100" & "00011" & "00011" & "0000000000000000"; -- beq com valores iguais
+            --wait for 100 ns;
 
-            instr <= "100011" & "01000" & "01001" & "0000000000100000";
+            --instr <= "100011" & "01000" & "01001" & "0000000000100000";
             
             wait;    
         end process;
-
-        --process
-        --begin
-                    
-         --end process;   
-        
-
-        
-
-        --process
-        --begin
-        --   wait for 5000 ns;
-        --   erro <= true;   -- Termina simulacao
-        --end process;
-        
-    --assert not erro report "Fim da Simulacao" severity failure;
     
 end behavior;
 
